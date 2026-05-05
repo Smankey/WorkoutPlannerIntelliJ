27801ee1486e3d5fffd3665feb85236bf7996ab6
Andreas Lindholm <lindholmandreas04@gmail.com>
2026-05-06 00:35:21 +0300

    Replace comments with single-line documentation style

--- WorkoutPlanner/src/model/ExerciseLibraryManager.java
+++ WorkoutPlanner/src/model/ExerciseLibraryManager.java
@@ -9,24 +9,18 @@ import java.io.IOException;
 import java.io.File;
 import java.util.List;
 
-/**
- * loading and saving exercises
- */
+//loading and saving exercises
 public class ExerciseLibraryManager {
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    /**
-     * reading the exercises from a list in a json-file.
-     */
+    // reading the exercises from a list in a json-file.
     public static List<Exercise> loadExercises() throws IOException {
         JsonNode root =  mapper.readTree(new File("exerciseLibrary/defaultExercises.json"));
         return mapper.treeToValue(root.get("exerciseLibrary"), new TypeReference<List<Exercise>>() {});
     }
 
-    /**
-     * saving the current exercise library to JSON format.
-     */
+    // saving the current exercise library to JSON format.
     public static void saveExercises(List<Exercise> exercises) throws IOException {
         ObjectNode root = mapper.createObjectNode();
         root.putPOJO("exerciseLibrary", exercises);
